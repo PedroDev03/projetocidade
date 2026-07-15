fd9708cdea349342269d52add793af85a27aa1ff
PedroDev03 <pedroaugustor03@gmail.com>
2026-07-15 20:42:50 -0300

    ajustes para build

--- projcidade/src/app/Componentes/CardCidade.tsx
+++ projcidade/src/app/Componentes/CardCidade.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 // Apenas importe os componentes que você realmente vai usar
-import { Box, Button, Card } from "@chakra-ui/react";
+import { Button, Card } from "@chakra-ui/react";
 
 
 
@@ -24,12 +24,6 @@ interface Cidades {
 
 }
 
-interface Lugar {
-    place_id: number;
-    name: string;
-    display_name: string;
-
-}
 
 
 export default function CardCidade() {
@@ -37,9 +31,6 @@ export default function CardCidade() {
     const [Data, setData] = useState<Cidades[]>([]);
     const [randomCity, setRandomCity] = useState<Cidades | null>(null);
     const [isLoading, setIsLoading] = useState(true);
-    const [isLoadingPharmacies, setIsLoadingPharmacies] = useState(false);
-    const [pharmacies, setPharmacies] = useState<Lugar[]>([]);
-    const [searchAttempted, setSearchAttempted] = useState(false);
     const [niche, setNiche] = useState<string>("Comércio");
 
     useEffect(() => {
@@ -75,46 +66,26 @@ export default function CardCidade() {
         if (Data.length > 0) {
             const randomIndex = Math.floor(Math.random() * Data.length);
             setRandomCity(Data[randomIndex]);
-            setPharmacies([]);
-            setSearchAttempted(false);
         }
     };
 
 
     const LinkDrogMap = <a target="blank" href={`https://www.google.com/maps/search/${niche}+${randomCity?.nome}+${randomCity?.microrregiao.mesorregiao.UF.sigla}`} style={{ color: 'indigo', fontWeight: 'bold' }} ><u> Clique aqui</u></a>
 
-
-    function cortarAtePalavra(
-        arr: Lugar[],
-        palavra: string
-    ): Lugar[] {
-        // procura o índice do primeiro objeto que contém a palavra
-        const index = arr.findIndex((item) =>
-            item.display_name.toLowerCase().includes(palavra.toLowerCase())
-        );
-
-        // se achou, retorna do começo até esse índice (excluindo o encontrado)
-        return index !== -1 ? arr.slice(0, index) : arr;
-    }
-
     if (isLoading) {
         return <p style={{ color: 'white' }}>Carregando lista de cidades...</p>;
     }
 
 
-
-    const pharmaciesCortadas = cortarAtePalavra(pharmacies, "Região");
     return (
         <>
 
 
-            <Box
-                bg="#5c161e"
-                minH="100vh"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-            >
+            <div style={{
+                backgroundColor: '#5c161e',
+                minHeight: '100vh',
+                display: 'flex'
+            }}>
                 <Card.Root
                     maxW="sm" overflow="hidden" marginTop={0} shadowColor={"black"}
                     boxShadow="xs"
@@ -153,44 +124,15 @@ export default function CardCidade() {
                             </div>
                         )}
 
-                        {searchAttempted && !isLoadingPharmacies && pharmacies.length === 0 && (
-                            <Text mt={4} color="orange.600">
-                                Nenhuma farmácia encontrada para esta cidade.
-                            </Text>
-                        )}
 
-
-
-                        {pharmacies && (
-
-                            pharmaciesCortadas.length > 0 && (
-                                <div style={{ marginTop: '20px' }}>
-                                    <Heading size="sm">Farmácias encontradas:  </Heading> <br></br>
-                                    <ul >
-                                        {pharmacies.map((lugar) => (
-
-                                            <li key={lugar.place_id} gap="2">
-                                                <b>Nome Drogaria:  </b>{lugar.name}<br></br>
-
-                                                <b>Endereço: </b> {lugar.display_name} <br></br>
-                                                <br></br>
-                                            </li>
-
-                                        ))}
-                                    </ul>
-                                </div>
-                            ))}
 
                     </Card.Body>
                     <Card.Footer gap="1" style={{ display: 'flex', justifyContent: 'center' }} >
                         <Button variant="solid" onClick={handleGetRandomCity} style={{ fontSize: '15px', textAlign: 'center' }} >Sortear uma cidade aleatória</Button>
-                        {/* <Button variant="ghost" colorScheme="teal" onClick={handleSearchPharmacies} >Ver drogarias</Button> */}
-                        {/* {(randomCity) &&
-                    <Button variant="ghost" colorScheme="teal" onClick={handleSearchPharmacies} style={{fontSize:'15px'}}>Ver drogarias</Button>
-                    }    */}
+
                     </Card.Footer>
                 </Card.Root>
-            </Box>
+            </div>
         </>
     );
 
--- projcidade/src/app/page.tsx
+++ projcidade/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 // Certifique-se de que o caminho abaixo bate exatamente com a sua pasta (maiúscula/minúscula)
-import Card from './Componentes/CardCidade'; 
+import Card from './componentes/cardcidade';
 
 export default function Home() {
   return (
